9432cd0ea026c3a7674c59f5e8fb6e3183e1dc50
12jaime12 <12jaimejcc12@gmail.com>
2023-04-04 14:44:18 +0200

    iteracion3

---
+++ JS_Basic_Algorithms/ITERACION3_OPERADORES.js
@@ -0,0 +1,16 @@
+//1.1
+let x = 10;
+let y = 5;
+console.log(x * y);
+//1.2
+let z = 2;
+console.log(x / z);
+//1.4
+x += 5;
+let o = x;
+console.log(o);
+//1.5
+x = 10;
+x *= 5;
+let i = x;
+console.log(i);
