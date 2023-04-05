8dd9667b473c03fb23afb6b3fdbe91d951215286
12jaime12 <12jaimejcc12@gmail.com>
2023-04-05 19:53:36 +0200

    iteracion 6

---
+++ JS_Functions/ITERACION6_VALORES_UNICOS.js
@@ -0,0 +1,18 @@
+const duplicates = [
+    'sushi',
+    'pizza',
+    'burger',
+    'potatoe',
+    'pasta',
+    'ice-cream',
+    'pizza',
+    'chicken',
+    'onion rings',
+    'pasta',
+    'soda'
+  ];
+  function removeDuplicates(array) {
+    let comidas = array => new Set(array);
+        return comidas(array);
+  }
+console.log(removeDuplicates(duplicates));
