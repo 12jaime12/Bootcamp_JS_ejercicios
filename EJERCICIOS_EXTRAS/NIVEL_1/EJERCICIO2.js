648ec414adc1e8d10d6fbf9f3b1f8e9fda0a5356
12jaime12 <12jaimejcc12@gmail.com>
2023-04-06 19:48:02 +0200

    ejercicio 2

---
+++ EJERCICIOS_EXTRAS/NIVEL_1/EJERCICIO2.js
@@ -0,0 +1,24 @@
+const fruits = ["Strawberry", "Banana", "Orange", "Apple"];
+const foodSchedule = [
+  { name: "Heura", isVegan: true },
+  { name: "Salmon", isVegan: false },
+  { name: "Tofu", isVegan: true },
+  { name: "Burger", isVegan: false },
+  { name: "Rice", isVegan: true },
+  { name: "Pasta", isVegan: true },
+];
+
+let veganas = (array1,array2) => {
+    array1.forEach(element => {
+        let i = 0;
+        if(element.isVegan === false){
+            element.name = array2[i];
+            element.isVegan = true;
+            array2.splice(i,1);
+            i++;
+        }
+        
+    });
+    console.log(array1);
+}
+veganas(foodSchedule,fruits);
