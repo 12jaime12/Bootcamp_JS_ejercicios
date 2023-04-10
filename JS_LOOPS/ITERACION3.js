6e7bdbf4baee2de249aa2d8c61ea6168ce615299
12jaime12 <12jaimejcc12@gmail.com>
2023-04-10 17:58:47 +0200

    ITERACION3

---
+++ JS_LOOPS/ITERACION3.js
@@ -0,0 +1,14 @@
+const placesToTravel = [
+  "Japon",
+  "Venecia",
+  "Murcia",
+  "Santander",
+  "Filipinas",
+  "Madagascar",
+];
+let bucleForOf = (array) => {
+  for (const pais of array) {
+    console.log(pais);
+  }
+};
+bucleForOf(placesToTravel);
