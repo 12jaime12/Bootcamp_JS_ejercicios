d11924b152cb8a855a3d39c5d83896d7a024ff4e
12jaime12 <12jaimejcc12@gmail.com>
2023-04-11 14:53:36 +0200

    EJERCICIO2.js

---
+++ EJERCICIOS_EXTRAS/NIVEL_2/EJERCICIO1.js
@@ -0,0 +1,32 @@
+const movies = [
+  {
+    title: "Bracula: Condemor II",
+    duration: 192,
+    categories: ["comedia", "aventura"],
+  },
+  {
+    title: "Spider-Man: No Way Home",
+    duration: 122,
+    categories: ["aventura", "acción"],
+  },
+  {
+    title: "The Voices",
+    duration: 223,
+    categories: ["comedia", "thriller"],
+  },
+  {
+    title: "Shrek",
+    duration: 111,
+    categories: ["comedia", "aventura", "animación"],
+  },
+];
+let ejercicio1 = (array) => {
+  let array2 = [];
+  for (const movie of array) {
+    movie.categories.forEach((element, index) => {
+      !array2.includes(element) && array2.push(element);
+    });
+  }
+  console.log(array2);
+};
+ejercicio1(movies);
